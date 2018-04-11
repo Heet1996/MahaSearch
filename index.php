6e5c0235a42c0fa09d46d3baf8f8d070da078832
Heet1996 <heet1@live.com>
2018-04-11 12:03:03 +0530

    added Config file

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <!-- mysql://b60ceb6df78c8a:de3eae1e@us-cdbr-iron-east-05.cleardb.net/heroku_a2e30006a7c23e2?reconnect=true -->
+<!-- mysql://b26e5c3445f854:92546290@us-cdbr-iron-east-05.cleardb.net/heroku_a2db3d44f93534b?reconnect=true -->
 <?php
     $host = 'us-cdbr-iron-east-05.cleardb.net';
-    $dbname = 'heroku_a2e30006a7c23e2';
-    $username = 'b60ceb6df78c8a';
-    $password = 'de3eae1e';
+    $dbname = 'heroku_a2db3d44f93534b';
+    $username = 'b26e5c3445f854';
+    $password = '92546290';
     
  
 try {
